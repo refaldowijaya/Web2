9d49eebbcdb8e5c0b299ab83b7c8fd82e02685d6
refaldowijaya <refaldowijaya22@gmail.com>
2026-04-20 22:37:13 +0700

    Delete latihan1.php

--- latihan1.php
+++
@@ -1,9 +0,0 @@
-<?php
-$A = 123; // variabel global [cite: 108]
-function Test() {
-    $A = "Test"; // variabel lokal [cite: 110]
-    echo "Nilai A dalam fungsi = $A \n"; [cite: 111]
-}
-Test(); [cite: 113]
-echo "Nilai A luar fungsi = $A \n"; [cite: 114]
-?>
