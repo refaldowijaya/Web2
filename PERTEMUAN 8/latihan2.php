f451bf31be3891e72a25575017d583b0de74c5c9
refaldowijaya <refaldowijaya22@gmail.com>
2026-04-21 19:33:20 +0700

    Add files via upload

---
+++ PERTEMUAN 8/latihan2.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head><title>Contoh Penggunaan UDF</title></head>
+<body>
+<!-- Menentukan Form Input -->
+<form method="POST">
+Masukkan Bilangan Pertama : <br>
+<input type="text" name="A" size=10> <br>
+Masukkan Bilangan Kedua : <br>
+<input type="text" name="B" size=10> <br>
+<input type="submit" value="hitung">
+</form>
+
+<!-- membandingkan 2 buah bilangan yang diinput -->
+<?php
+if (isset($_POST["A"]) && isset($_POST["B"])) {
+    $A = $_POST["A"];
+    $B = $_POST["B"];
+    
+    function jumlah($A, $B) {  
+        $jumlahbil = $A + $B;
+        return $jumlahbil;
+    }
+    
+    function kurang($A, $B) {  
+        $kurangbil = $A - $B;
+        return $kurangbil;
+    }
+    
+    function kali($A, $B) {
+        $kalibil = $A * $B;
+        return $kalibil;
+    }
+    
+    function bagi($A, $B) {
+        // Untuk menghindari division by zero
+        if ($B != 0) {
+            $bagibil = $A / $B;
+        } else {
+            $bagibil = 0;
+        }
+        return $bagibil;
+    }
+    
+    echo "<br>";
+    echo ("Bilangan Pertama : ");
+    echo $A;
+    echo "<br>";
+    echo ("Bilangan Kedua : ");
+    echo $B;
+    echo "<br><br>";
+    
+    echo "Hasil Penjumlahan 2 buah bilangan ";
+    echo "<br>";
+    $jumlahbil = jumlah($A, $B);
+    printf("Penjumlahan antara : %d + %d = %d ", $A, $B, $jumlahbil);
+    echo "<br><br>";
+    
+    echo "Hasil Pengurangan 2 buah bilangan ";
+    echo "<br>";
+    $kurangbil = kurang($A, $B);
+    printf("Pengurangan antara : %d - %d = %d ", $A, $B, $kurangbil);
+    echo "<br><br>";
+    
+    echo "Hasil Perkalian 2 buah bilangan ";
+    echo "<br>";
+    $kalibil = kali($A, $B);
+    printf("Perkalian antara : %d * %d = %d ", $A, $B, $kalibil);
+    echo "<br><br>";
+    
+    echo "Hasil Pembagian 2 buah bilangan ";
+    echo "<br>";
+    if ($B != 0) {
+        $bagibil = bagi($A, $B);
+        printf("Pembagian antara : %d / %d = %d ", $A, $B, $bagibil);
+    } else {
+        echo "Pembagian antara : $A / $B = Tak Terdefinisi (nol)";
+    }
+    echo "<br><br>";
+}
+?>
+</body>
+</html>
